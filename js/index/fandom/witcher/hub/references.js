7694ee3dcf78dcb4afbe1af63d044117dadd2a88
vordac <sashaburgazli@gmail.com>
2021-12-15 14:41:12 +0200

    created nat

---
+++ js/index/fandom/witcher/hub/references.js
@@ -0,0 +1,12 @@
+function goIndex(){
+    document.location.href = "../../index.html";
+}
+function goHeroes(){
+    document.location.href = "character.html";
+}
+function goMap(){
+    document.location.href = "map.html";
+}
+function goLore(){
+    document.location.href = "lore.html";
+}
