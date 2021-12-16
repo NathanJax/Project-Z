ad114873df8d1a565135d1411e5ba596b9c9182a
vordac <sashaburgazli@gmail.com>
2021-12-16 11:25:10 +0200

    added characters

---
+++ js/index/fandom/witcher/character/display_info.js
@@ -0,0 +1,88 @@
+function displayGeralt(){
+    document.getElementById("geralt-info").style.display="block";
+}
+
+function closeGeralt(){
+    document.getElementById("geralt-info").style.display="none";
+}
+
+function displayCiri(){
+    document.getElementById("ciri-info").style.display="block";
+}
+
+function closeCiri(){
+    document.getElementById("ciri-info").style.display="none";
+}
+
+
+function displayYennefer(){
+    document.getElementById("yennefer-info").style.display="block";
+}
+
+function closeYennefer(){
+    document.getElementById("yennefer-info").style.display="none";
+}
+
+function displayTriss(){
+    document.getElementById("triss-info").style.display="block";
+}
+
+function closeTriss(){
+    document.getElementById("triss-info").style.display="none";
+}
+
+function displayJaskier(){
+    document.getElementById("jaskier-info").style.display="block";
+}
+
+function closeJaskier(){
+    document.getElementById("jaskier-info").style.display="none";
+}
+
+function displayVesemir(){
+    document.getElementById("vesemir-info").style.display="block";
+}
+
+function closeVesemir(){
+    document.getElementById("vesemir-info").style.display="none";
+}
+
+function displayEskel(){
+    document.getElementById("eskel-info").style.display="block";
+}
+
+function closeEskel(){
+    document.getElementById("eskel-info").style.display="none";
+}
+
+function displayLambert(){
+    document.getElementById("lambert-info").style.display="block";
+}
+
+function closeLambert(){
+    document.getElementById("lambert-info").style.display="none";
+}
+
+function displayVampire(){
+    document.getElementById("vampire-info").style.display="block";
+}
+
+function closeVampire(){
+    document.getElementById("vampire-info").style.display="none";
+}
+
+function displayZoltan(){
+    document.getElementById("zoltan-info").style.display="block";
+}
+
+function closeZoltan(){
+    document.getElementById("zoltan-info").style.display="none";
+}
+
+function displayVilgefortz(){
+    document.getElementById("vilgefortz-info").style.display="block";
+}
+
+function closeVilgefortz(){
+    document.getElementById("vilgefortz-info").style.display="none";
+}
